9ec5a58cf82fce9167e23bd655788674f563f874
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2021-05-05 20:51:25 +0800

    Initialize project for Prehospital app documentation

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("/home/ajipandean/Public/Sandbox/docusaurus-test/node_modules/remark-admonitions/styles/infima.css"),
+  require("/home/ajipandean/Public/Sandbox/docusaurus-test/node_modules/remark-admonitions/styles/infima.css"),
+  require("/home/ajipandean/Public/Sandbox/docusaurus-test/node_modules/remark-admonitions/styles/infima.css"),
+  require("/home/ajipandean/Public/Sandbox/docusaurus-test/node_modules/infima/dist/css/default/default.css"),
+  require("/home/ajipandean/Public/Sandbox/docusaurus-test/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/ajipandean/Public/Sandbox/docusaurus-test/src/css/custom.css"),
+];
